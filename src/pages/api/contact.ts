d966e83c9549a28401694836e78a575fe58d6235
hugo-glt <hg.gilet@gmail.com>
2026-09-09 23:03:32 +0200

    ADD: Domaine Name

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -66,7 +66,7 @@ function escapeHtml(str: string): string {
 export const POST: APIRoute = async ({ request, clientAddress }) => {
     try {
     const origin = request.headers.get('origin');
-    const allowedOrigin = 'http://localhost:4321'; // Remplacez par domaine de production
+    const allowedOrigin = 'https://hugo-gilet.fr'; // Remplacez par domaine de production
 
     if (origin && origin !== allowedOrigin) {
         return new Response(
