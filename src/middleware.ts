a6d5228b6f3863dfbccf6d85be357d5960f4f1e6
hugo-glt <hg.gilet@gmail.com>
2026-09-11 10:56:25 +0200

    FIX: Security update (CSP)

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,10 +6,6 @@ export const onRequest = defineMiddleware(async (context, next) => {
     response.headers.set('X-Content-Type-Options', 'nosniff');
     response.headers.set('X-Frame-Options', 'DENY');
     response.headers.set('Referrer-Policy', 'strict-origin-when-cross-origin');
-    response.headers.set(
-        'Content-Security-Policy',
-        "default-src 'self'; img-src 'self'; style-src 'self'; script-src 'self'; connect-src 'self'; font-src 'self'; base-uri 'self'; form-action 'self'"
-    );
     response.headers.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains');
 
     return response;
